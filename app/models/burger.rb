e3a8fa2a8c3d7dfb43f4c46b4db0e03c12207725
loic de deyn <loicdedeyn.a@gmail.com>
2023-02-27 17:23:42 +0100

    adding dependencies to models

--- app/models/burger.rb
+++ app/models/burger.rb
@@ -1,3 +1,4 @@
 class Burger < ApplicationRecord
   belongs_to :user
+  has_many :bookings, :reviews, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_27_161314) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_27_161752) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.bigint "burger_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["burger_id"], name: "index_bookings_on_burger_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "burgers", force: :cascade do |t|
     t.string "name"
@@ -21,6 +32,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_161314) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_burgers_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.float "rating"
+    t.bigint "burger_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["burger_id"], name: "index_reviews_on_burger_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -36,5 +58,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_161314) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "burgers"
+  add_foreign_key "bookings", "users"
   add_foreign_key "burgers", "users"
+  add_foreign_key "reviews", "burgers"
+  add_foreign_key "reviews", "users"
 end

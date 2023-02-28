a831e7db67b1dd9fe13fe2a9ad1a9271ddae8bff
Davy De Groote <davy.degr@gmail.com>
2023-02-28 15:05:07 +0100

    Added reviews to be displayed on burger showpage

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,11 @@
 class Review < ApplicationRecord
   belongs_to :burger
   belongs_to :user
+
+  validates :content, presence: true
+  validates :content, length: { minimum: 10 }
+
+  validates :rating, presence: true
+  validates :rating, numericality: { greater_than: 0, less_than_or_equal_to: 5 }
+
 end

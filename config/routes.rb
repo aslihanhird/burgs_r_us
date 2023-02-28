65a2d2cf38e958a9e2675b06d97dd40db44a7469
loic de deyn <loicdedeyn.a@gmail.com>
2023-02-28 14:13:22 +0100

    adding the correct routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :burgers, only: %i[index show new create] do
     resources :reviews, only: %i[new create]
+    resources :bookings, only: %i[new create]
   end
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html

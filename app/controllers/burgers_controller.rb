3dec5c015b8e5d2871ec75ad1bf2af8cb70aaf63
loic de deyn <loicdedeyn.a@gmail.com>
2023-02-28 11:49:04 +0100

    adding show, new, create action and views

--- app/controllers/burgers_controller.rb
+++ app/controllers/burgers_controller.rb
@@ -4,4 +4,29 @@ class BurgersController < ApplicationController
     @burgers = Burger.all
   end
 
+  def show
+    @burger = Burger.find(params[:id])
+  end
+
+  def new
+    @burger = Burger.new
+  end
+
+  def create
+    @burger = Burger.new(burger_params)
+    @current_user = current_user
+    @burger.user_id = current_user.id
+    if @burger.save
+      redirect_to burger_path(@burger)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def burger_params
+    params.require(:burger).permit(:name, :description, :user_id)
+  end
+
 end

9a8a86973d19955943b806eb06a00dcaae2797f2
loic de deyn <loicdedeyn.a@gmail.com>
2023-02-28 14:40:45 +0100

    adding the create, new and show action + views associated

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,35 @@
+class BookingsController < ApplicationController
+
+  before_action :set_burger, only: %i[new create show]
+
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user_id = current_user.id
+    @booking.burger = @burger
+    if @booking.save
+      redirect_to burger_path(@burger)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  private
+
+  def set_burger
+    @burger = Burger.find(params[:burger_id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
+
+end

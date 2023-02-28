27461e6deeefa42758bb99a7826c6a0b07d847d2
Aslihan Hird <aslihan.hird@gmail.com>
2023-02-28 16:30:05 +0100

    fixed routing error

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'burgers#index'
   resources :burgers, only: %i[show new create] do
     resources :reviews, only: %i[new create]
     resources :bookings, only: %i[new create show]
   end
   resources :bookings, only: %i[index]
-  root to: "pages#home"
+  root to: "burgers#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

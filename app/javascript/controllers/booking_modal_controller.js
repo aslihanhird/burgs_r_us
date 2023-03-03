1c02e544ce1bc2f750e802db1f08cd5db2ca3ea4
Aslihan Hird <aslihan.hird@gmail.com>
2023-03-03 15:02:28 +0100

    start

--- app/javascript/controllers/booking_modal_controller.js
+++ app/javascript/controllers/booking_modal_controller.js
@@ -6,18 +6,7 @@ export default class extends Controller {
 
   connect() {
     const myModal = new bootstrap.Modal(document.getElementById('exampleModal'));
-    // const startValue = this.element.startTarget;
-    // const booking = params[:booking_id],
-    // const modal_toggle = @modal_toggle,
-    console.log('Hi asli!');
-    console.log('the controller:' ,this.element);
-    console.log(myModal)
 
     myModal.show()
-
-  }
-
-  send(event) {
-    // event.preventDefault()
   }
 }
